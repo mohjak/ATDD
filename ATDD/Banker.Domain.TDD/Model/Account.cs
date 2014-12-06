bcb99ea6ede73326c1083c13ced948973801d59b
mohjak <mohjak@gmail.com>
2014-12-06 23:58:23 +0200

    TDD Withdraw

---
+++ ATDD/Banker.Domain.TDD/Model/Account.cs
@@ -0,0 +1,8 @@
+namespace Banker.Domain.TDD.Model
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ ATDD/Banker.Domain.TDD/Services/AccountantService.cs
@@ -0,0 +1,26 @@
+﻿using Banker.Domain.TDD.Infrastructure;
+using Banker.Domain.TDD.Model;
+
+namespace Banker.Domain.TDD.Services
+{
+    public class AccountantService
+    {
+        private IAccountsRepository _accountRepo;
+        private ITransactionRepository _transactionRepo;
+
+        public AccountantService(IAccountsRepository accountRepo, ITransactionRepository transactionRepo)
+        {
+            _accountRepo = accountRepo;
+            _transactionRepo = transactionRepo;
+        }
+
+        public Account Withdraw(int accountId, decimal amount)
+        {
+            Account account = _accountRepo.Get(accountId);
+            account.Balance -= amount;
+            _accountRepo.Update(account);
+            _transactionRepo.Create(accountId, amount);
+            return account;
+        }
+    }
+}
